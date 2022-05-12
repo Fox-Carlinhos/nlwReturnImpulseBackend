b035afadc3a1814a799b18453610c3b21a334c1b
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2022-05-12 10:42:50 -0300

    ServerRepo

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "543aa42be09886",
+    pass: "14a0a65da5e9ee",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Carlos Alberto Jr <carlosalberto.cfjr@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
